2748f1974fb44d057840341a1a10e0d114a19168
Lakshya <lakshyasatpal372@gmail.com>
2021-07-16 21:25:07 +0530

    Added process.env.PORT in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 80;
+const port = process.env.PORT || 80;
 const staticPath = path.join(__dirname, "../public");
 app.use(express.static(staticPath));
 
